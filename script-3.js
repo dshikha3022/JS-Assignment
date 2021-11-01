1bfe8ad24ac07a1b1320b4e457760389f56ba91d
dshikha3022 <83084902+dshikha3022@users.noreply.github.com>
2021-11-01 22:04:05 +0530

    Create script-3.js

---
+++ script-3.js
@@ -0,0 +1,16 @@
+//UNDEFINED 
+var a ;
+if (a == undefined){
+	console.log("a is undefined variable");
+}
+
+//DEFINED
+var a = 10 ;
+if (a == undefined){
+	console.log("a is undefined variable");
+}else{
+	console.log("a is defined variable and its value is  "+ a);
+}
+
+//NOT DEFINED
+console.log(z);
