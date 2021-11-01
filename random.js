c5ce347bcd6986167e4909e04b0253ceb5c91660
dshikha3022 <83084902+dshikha3022@users.noreply.github.com>
2021-11-01 23:08:01 +0530

    Update random.js

--- random.js
+++ random.js
@@ -1,12 +1,25 @@
-function un_defined() {
-  var a;
-  console.log(a);
-  a = "Before assigning value if we call a variable, it shows UNDEFINED.";
-  console.log(a);
+let a = 4;
+function link1 () {
+    document.getElementById("demo1").innerHTML = "Let a=4 be a global variable";
 }
-function not_defined() {
-  console.log(
-    "When we did not declare the variable and try to call that variable, it shows NOT-DEFINED."
-  );
-  console.log(b);
+
+function link2(){
+    a = 3;
+    document.getElementById("demo2").innerHTML = "The same global variable is now changed to 3 inside this function.";
+    console.log(a);
+}
+
+function link3() {
+    b = 5;
+    document.getElementById("demo3").innerHTML = "A variable b=5 is used without declaring it so it's global by default.";
+    console.log(b);
+}
+
+// program showing local scope of a variable
+let c = 6;
+
+function local() {
+    let d = 7;
+    document.getElementById("demo4").innerHTML = "This is an example of a Local Variable being accessed. Sum = 13";
+    console.log(c+d);
 }
